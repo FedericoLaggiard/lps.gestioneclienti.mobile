524a57219654d142d7b3ebe0436154c69997bd6e
federicolaggiard <federico.laggiard@spindox.it>
2017-02-02 22:07:24 +0100

    fix spinner bg

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -18,20 +18,26 @@ Spinner.controller = function() {
 Spinner.view = function(ctrl) {
 
   if (this.visible()) {
-    return m('svg', {
-        className: 'spinner',
-        viewBox: "0 0 66 66",
-        xmlns: "http://www.w3.org/2000/svg"
-      }, m('circle', {
-        className: 'circle',
-        fill: 'none',
-        "stroke-width": "6",
-        "stroke-linecap": "round",
-        cx: "33",
-        cy: "33",
-        r: "30"
-      })
-    )
+    return m('div',{
+      className: 'spinner-container'
+    },[
+      m('svg', {
+          className: 'spinner',
+          viewBox: "0 0 66 66",
+          xmlns: "http://www.w3.org/2000/svg"
+        }, m('circle', {
+          className: 'circle',
+          fill: 'none',
+          "stroke-width": "6",
+          "stroke-linecap": "round",
+          cx: "33",
+          cy: "33",
+          r: "30"
+        })
+      )
+    ])
+
+
   }
 
   return '';
